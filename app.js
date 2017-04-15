9b3289306fbdb8c3f29fcc3d575341340030e56e
Shiva Kayathi <kaya0018@algonquinlive.com>
2017-04-15 16:57:36 -0400

    fix comments

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 var app = angular.module('app', []);
 
-/**********************************************************************************
- * Exercise 2                                                                     *
+/**********************************************************************************                                                              *
  * Use of an external JSON API to search for movies and series.                   *
  *********************************************************************************/
 
 /**
  * OMDb API: http://www.omdbapi.com/
- * 
- * TODO: Complete the service to call the OMDb API:
- * - Do a searchMovies(keywords) function that will call the OMDb API and return
- *   the list of movies corresponding to the keywords.
- *
- * Some remarks:
- * - Minimize the number of API calls when it's possible (cache results, etc.).
- * - No need to use external modules, angular services are enough: $http, $q, etc.
- * - Don't hesitate to use console.log() for the received movies to see how is formated the JSON
- *   and the different movies attributes that you can use.
  */
 app.provider('OMDbApi', function OMDbApiProvider() {
   var endpoint = 'http://www.omdbapi.com/';
@@ -25,7 +14,7 @@ app.provider('OMDbApi', function OMDbApiProvider() {
   
   this.$get = ['$http', function OMDbApiFactory($http) {
       return {
-        // TODO
+        // function that will call the OMDb API and return the list of movies corresponding to the keywords.
         searchMovies: function(keywords){
            return $http.get("http://www.omdbapi.com/?s="+keywords);          
         }
@@ -35,34 +24,27 @@ app.provider('OMDbApi', function OMDbApiProvider() {
 
 /**
  * Movie Controller
- *
- * TODO: Handle the search field and display the result
- * - You can choose how to trigger the display of the result (click on a button, timeout, etc.).
- * - Display a message if there is no result.
- * - Each line of result have to be clickable to redirect to the imdb.com movie page.
- *
- * Bonus:
- * - Allow to be able to sort the movies by release date (and change the sorting by a button)
- * - Display movies with a different color than series
  */
 app.controller('MovieController', ['$scope','$http', 'OMDbApi',
   function ($scope, $http, OMDbApi)
   {
     $scope.sortReverse = false;
-    // TODO
+    // This function will retrieve all movies names based on user entered keyword
     $scope.searchMovieName = function() {
+      if(undefined !== $scope.search && $scope.search.length > 0){
       OMDbApi.searchMovies($scope.search).then(  
         function(response){
-          debugger;
           if(response.data.Response === "True"){
             $scope.movies =  response.data.Search;
             $scope.showError = false;
             console.log($scope.movies);
           }else{
             $scope.showError = true;
+            $scope.movies = [];
           }
           
         }
       );
+    }
     }
 }]);
